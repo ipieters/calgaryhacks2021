bd8d62aeeefadc2e7fdfa2a360e20040a34c875d
Isabella Guimet <isabellaguimet@hotmail.com>
2021-02-14 10:38:42 -0700

    Added design for dashboard page

--- calgary-hacks-2021/src/components/Dashboard.js
+++ calgary-hacks-2021/src/components/Dashboard.js
@@ -55,50 +55,78 @@ function Dashboard() {
     return(
         <div>
             <NavigationBar />
+            <div className="main">
             {/* { <button onClick={() => addInCourse({cname : "CPSC 457", User : "Julio Agostini" })}>
             </button> } */}
             <h1>Dashboard</h1>
-            <Container>
+            <Container class="main-container">
 				<Row>
 					<Col>
-						<ListGroup as="ul">
+            <div className="classes-left-container">
+            <ListGroup as="ul">
 							<ListGroup.Item as="li" active>
-								Cras justo odio
+								Classes
 							</ListGroup.Item>
-							<ListGroup.Item as="li">Dapibus ac facilisis in</ListGroup.Item>
-							<ListGroup.Item as="li" disabled>
-								Morbi leo risus
-							</ListGroup.Item>
-							<ListGroup.Item as="li">Porta ac consectetur ac</ListGroup.Item>
+							<ListGroup.Item as="li">CPSC 433</ListGroup.Item>
+							<ListGroup.Item as="li" disabled>CPSC 413</ListGroup.Item>
+							<ListGroup.Item as="li">DINO 345</ListGroup.Item>
 						</ListGroup>
+            </div>
+						
 					</Col>
 					<Col>
-						<ListGroup as="ul">
-							<ListGroup.Item as="li" active>
-								Cras justo odio
-							</ListGroup.Item>
-							<ListGroup.Item as="li">Dapibus ac facilisis in</ListGroup.Item>
-							<ListGroup.Item as="li" disabled>
-								Morbi leo risus
-							</ListGroup.Item>
-							<ListGroup.Item as="li">Porta ac consectetur ac</ListGroup.Item>
-						</ListGroup>
+          <section className="middle-section">
+          <div className="class-middle-container">
+              <div className="course-text-middle">
+              <h2 id="course-name">Course Name</h2>
+             <h3 id="course-code-info">CPSC 413 - Design of Noodles</h3>
+             <p>In this class, we learn how to make delicious pasta!</p>
+              </div>
+             
+             <div className="container-people"></div>
+             <div className="class-people">
+                <h3 className="text-classes">People in Group</h3>
+                <p>
+                  Igor <br></br>
+                  Michelle <br></br>
+                  Julio <br></br>
+                  Isa <br></br>
+                </p>
+              </div>
+             <div className="interest-people">
+             <h3 className="text-classes">Group Interests</h3>
+             <p>
+               Hiking <br></br>
+               Basketball <br></br>
+               Gaming <br></br>
+               Travelling <br></br>
+             </p>
+             <div>
+             </div>
+             </div>
+            </div> 
+          </section>
+         
+
 					</Col>
 					<Col>
-						<ListGroup as="ul">
+            <div className="friends-right-container">
+            <ListGroup as="ul">
 							<ListGroup.Item as="li" active>
-								Cras justo odio
+								List of Friends
 							</ListGroup.Item>
-							<ListGroup.Item as="li">Dapibus ac facilisis in</ListGroup.Item>
-							<ListGroup.Item as="li" disabled>
-								Morbi leo risus
+							<ListGroup.Item as="li">Luigi</ListGroup.Item>
+							<ListGroup.Item as="li">
+								Birdo
 							</ListGroup.Item>
-							<ListGroup.Item as="li">Porta ac consectetur ac</ListGroup.Item>
+							<ListGroup.Item as="li">Bowser</ListGroup.Item>
 						</ListGroup>
+            </div>
+						
 					</Col>
 				</Row>
 			</Container>
-            <Container></Container>
+         {/*   <Container></Container>
             <Container></Container>
             {courses.map((course) => (
             <div> <h2>Name {course.name} </h2>
@@ -112,8 +140,10 @@ function Dashboard() {
             <p></p>
             </div>
           ))}
-          
+            */}
+            </div>
         </div>
+        
     );
 }
 
