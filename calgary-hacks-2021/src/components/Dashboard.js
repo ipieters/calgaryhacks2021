ebc876f15091ee6c98b16298feebe78ec6a07056
jagos97 <agostini98@gmail.com>
2021-02-13 23:42:08 -0700

    dashboard works

--- calgary-hacks-2021/src/components/Dashboard.js
+++ calgary-hacks-2021/src/components/Dashboard.js
@@ -1,12 +1,60 @@
 import React from 'react';
 import Nav from "./Nav";
+import{ useState, useEffect, Fragment } from "react";
+import 'firebase/firestore';
+import firebase from '../firebase';
+
 
 function Dashboard() {
+    const [courses, setCourses] = useState([]);
+    const [incourse, setInCourses] = useState([]);
+
+    const refc = firebase.firestore().collection("Courses");
+    const refi = firebase.firestore().collection("InCourses");
+
+    function getCourses(){
+        refc.onSnapshot( (querySnapshot) => {
+          const items = [];
+          querySnapshot.forEach((doc) => {
+            items.push(doc.data());
+          });
+          setCourses(items)
+        });
+    }
+
+    function getInCourses(){
+        refi.onSnapshot( (querySnapshot) => {
+          const items2 = [];
+          querySnapshot.forEach((doc) => {
+            items2.push(doc.data());
+          });
+          setInCourses(items2)
+        });
+    }
+
+    useEffect(() => {
+        getCourses();
+        getInCourses();
+        // eslint-disable-next-line
+      }, []);
 
     return(
         <div>
             <Nav />
             <h1>Dashboard</h1>
+            {courses.map((course) => (
+            <div> <h1>Name {course.name} </h1>
+            <h2>Description: {course.desc}</h2>
+            <p></p>
+            </div>
+          ))}
+            {incourse.map((inc) => (
+            <div>
+            <h2>{inc.user} is in {inc.cname}</h2>
+            <p></p>
+            </div>
+          ))}
+          
         </div>
     );
 }
