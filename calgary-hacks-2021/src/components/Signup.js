820dee8386cf18138fc7cf849cd36c5b9c01e88c
Isabella Guimet <isabellaguimet@hotmail.com>
2021-02-13 18:21:23 -0700

    sign up page

--- calgary-hacks-2021/src/components/Signup.js
+++ calgary-hacks-2021/src/components/Signup.js
@@ -1,16 +1,18 @@
 import React, { useRef, useState } from "react"
 import { Form, Button, Card, Alert } from "react-bootstrap"
+import { useAuth } from "../contexts/AuthContext"
 
 export default function Signup() {
     const emailRef = useRef()
     const passwordRef = useRef()
     const passwordConfirmRef = useRef()
-    const [loading, setLoading] = useState(false)
+    const { signup } = useAuth()
+    //const [loading, setLoading] = useState(false)
     /*
   const emailRef = useRef()
   const passwordRef = useRef()
   const passwordConfirmRef = useRef()
-  //const { signup } = useAuth()
+  //
   const [error, setError] = useState("")
   const [loading, setLoading] = useState(false)
   //const history = useHistory()
@@ -53,7 +55,7 @@ export default function Signup() {
               <Form.Label>Password Confirmation</Form.Label>
               <Form.Control type="password" ref={passwordConfirmRef} required />
             </Form.Group>
-            <Button disabled={loading} className="w-100" type="submit">
+            <Button className="w-100" type="submit">
               Sign Up
             </Button>
           </Form>
